21805f03b06650cb764e7b10e3ef2fd9da929dd0
Vladislav <vladislav.belyaev.93@gmail.com>
2017-03-01 22:54:34 +0200

    Update carriervawe S3 config.

--- config/initializers/carriervawe.rb
+++ config/initializers/carriervawe.rb
@@ -1,16 +1,28 @@
 CarrierWave.configure do |config|
   rails_secrets = Rails.application.secrets
 
-  unless Rails.env.test?
+  # Use secrets.yml on development
+  if Rails.env.development?
     config.fog_credentials = {
         :provider              => 'AWS',
         :aws_access_key_id     => rails_secrets.S3_KEY,
         :aws_secret_access_key => rails_secrets.S3_SECRET,
         :region                => rails_secrets.S3_REGION,
         endpoint:              rails_secrets.S3_PATH
-        # host:                  's3.example.com',             # optional, defaults to nil
-
     }
+    config.fog_directory = rails_secrets.S3_BUCKET_NAME
+  end
+
+  # Use heroku vars on production
+  if Rails.env.production?
+    config.fog_credentials = {
+        provider:              'AWS',
+        aws_access_key_id:     ENV['S3_KEY'],
+        aws_secret_access_key: ENV['S3_SECRET'],
+        region:                ENV['S3_REGION'],
+        endpoint:              ENV['S3_PATH']
+    }
+    config.fog_directory = ENV['S3_BUCKET_NAME']
   end
 
   # For testing, upload files to local `tmp` folder.
@@ -23,7 +35,5 @@ CarrierWave.configure do |config|
   end
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
-
-  config.fog_directory    = rails_secrets.S3_BUCKET_NAME
-  Fog::Storage.new(config.fog_credentials).sync_clock
+  Fog::Storage.new(config.fog_credentials).sync_clock if Rails.env.development?
 end
